289a69b2b5eb233ba4e6b05851fed544653d947b
STX Developer <stx@trading.local>
2025-12-31 00:30:07 +0100

    Initial commit - LinkedIn AI Auto Job Applier customized by STX MINUS

---
+++ modules/resumes/extractor.py
@@ -0,0 +1,18 @@
+'''
+Author:     STX MINUS
+WhatsApp:   +234 813 732 2932
+Location:   Nigeria
+Role:       Software Developer
+
+License:    GNU Affero General Public License
+            https://www.gnu.org/licenses/agpl-3.0.en.html
+
+version:    25.01.01
+'''
+
+
+from config.personals import *
+from config.questions import default_resume_path
+
+
+
